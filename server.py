bb4d7eec05315e481ec1057d164b845cd34ef3b1
atyoshi <yoshi1473@hotmail.com>
2025-07-23 14:56:51 -0400

    error handling and static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+This module provides a Flask web application for detecting emotions in text.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,18 +8,30 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emo_detector():
+    """
+    Analyzes the emotion of the provided text and returns the results.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
 
-    output = f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, "
-    fearjoysad = f"'fear': {response['fear']}, 'joy': {response['joy']}, 'sadness': {response['sadness']}. "
-    dominant = f"The dominant emotion is {response['dominant_emotion']}."
+    if response['dominant_emotion'] is not None:
+        output = (
+            f"For the given statement, the system response is 'anger': {response['anger']}, "
+            f"'disgust': {response['disgust']}, "
+            f"'fear': {response['fear']}, 'joy': {response['joy']}, "
+            f"'sadness': {response['sadness']}. "
+            f"The dominant emotion is {response['dominant_emotion']}."
+        )
+        return output
 
-    return output + fearjoysad + dominant
+    return "Invalid text! Please try again!"
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the index.html page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
